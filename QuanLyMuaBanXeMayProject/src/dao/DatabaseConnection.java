2ddc222707610d02fd158b46ddc757f817b06781
LongS7 <longkeyvic1@gmail.com>
2020-06-22 17:37:03 +0700

    long

--- QuanLyMuaBanXeMayProject/src/dao/DatabaseConnection.java
+++ QuanLyMuaBanXeMayProject/src/dao/DatabaseConnection.java
@@ -5,10 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+	public static String userName = "";
+	public static String password = "";
 	public static Connection getConnection() throws SQLException {
 		String url = "jdbc:jtds:sqlserver://localhost:1433;QLBX;instance=SQLEXPRESS";
 		
-		Connection conn = DriverManager.getConnection(url, "admin", "1530");
+		Connection conn = DriverManager.getConnection(url, userName, password);
 		
 		return conn;
 	}
