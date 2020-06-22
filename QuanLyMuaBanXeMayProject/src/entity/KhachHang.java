8149f49ff47a8900c00ee820efdd452b34470e2b
HoangXuanKhang <45003638+HoangXuanKhang@users.noreply.github.com>
2020-06-22 18:36:13 +0700

    khang

--- QuanLyMuaBanXeMayProject/src/entity/KhachHang.java
+++ QuanLyMuaBanXeMayProject/src/entity/KhachHang.java
@@ -79,6 +79,31 @@ public class KhachHang {
 		return "KhachHang [maKH=" + maKH + ", hoTenKH=" + hoTenKH + ", gioiTinhKH=" + gioiTinhKH + ", diaChiKH="
 				+ diaChiKH + ", sdtKH=" + sdtKH + ", emailKH=" + emailKH + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((maKH == null) ? 0 : maKH.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KhachHang other = (KhachHang) obj;
+		if (maKH == null) {
+			if (other.maKH != null)
+				return false;
+		} else if (!maKH.equals(other.maKH))
+			return false;
+		return true;
+	}
 	
 	
 }
